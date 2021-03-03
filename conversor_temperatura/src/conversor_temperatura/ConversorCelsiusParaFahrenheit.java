7b8f82a40f42d94c1434d2eeee140816ccdb8f94
Eveh <evelyn.lavignes@gmail.com>
2021-03-03 11:19:00 -0300

    aula 03-03-2021 - Turma A

---
+++ conversor_temperatura/src/conversor_temperatura/ConversorCelsiusParaFahrenheit.java
@@ -0,0 +1,24 @@
+package conversor_temperatura;
+
+public class ConversorCelsiusParaFahrenheit {
+
+public static void main(String[] args) {
+	
+	float celsius;
+	float fahrenheit;
+	
+	celsius = 35;
+	fahrenheit = (9 * celsius + 160)/5;
+	
+System.out.println(fahrenheit);
+System.out.println(celsius + "graus celsius equivale a " + fahrenheit + " graus fahrenheit");
+
+
+	
+		
+	
+  }	
+	
+}
+
+
